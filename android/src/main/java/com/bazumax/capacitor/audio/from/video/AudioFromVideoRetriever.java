d7d5ad777626e745502b3d90b76a37307fa7e4b4
Maxim Bazuev <bazuka5801@mail.ru>
2023-10-19 20:30:11 +0300

    feat: android implementation

--- android/src/main/java/com/bazumax/capacitor/audio/from/video/AudioFromVideoRetriever.java
+++ android/src/main/java/com/bazumax/capacitor/audio/from/video/AudioFromVideoRetriever.java
@@ -1,11 +1,152 @@
 package com.bazumax.capacitor.audio.from.video;
 
-import android.util.Log;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.media.MediaCodec;
+import android.media.MediaExtractor;
+import android.media.MediaFormat;
+import android.net.Uri;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 
 public class AudioFromVideoRetriever {
 
-    public String echo(String value) {
-        Log.i("Echo", value);
-        return value;
+
+    public interface ExtractionCallback {
+        void onExtractionCompleted(File audioFile);
+        void onExtractionFailed(String errorMessage);
+    }
+
+    public File getFileObject(String path, ContentResolver resolver) {
+        Uri u = Uri.parse(path);
+        if (u != null && "content".equals(u.getScheme())) {
+            Cursor cursor = resolver.query(u, new String[]{android.provider.MediaStore.Images.ImageColumns.DATA}, null, null, null);
+            cursor.moveToFirst();
+            String filePath = cursor.getString(0);
+            cursor.close();
+            return new File(filePath);
+        }
+        if (u.getScheme() == null || u.getScheme().equals("file")) {
+            return new File(u.getPath());
+        }
+        return null;
+    }
+
+    public void extractAudio(File videoFile, File outputAudioFile, ExtractionCallback callback) {
+        MediaExtractor extractor = new MediaExtractor();
+
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(videoFile);
+            FileDescriptor fd = fis.getFD();
+            extractor.setDataSource(fd);
+        } catch (IOException e) {
+            callback.onExtractionFailed("Failed to set data source: " + e.getMessage());
+            return;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //Release stuff
+            try {
+                if(fis != null) {
+                    fis.close();
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        int audioTrackIndex = findAudioTrack(extractor);
+        if (audioTrackIndex == -1) {
+            callback.onExtractionFailed("No audio track found in the video");
+            return;
+        }
+
+        extractor.selectTrack(audioTrackIndex);
+        MediaFormat audioFormat = extractor.getTrackFormat(audioTrackIndex);
+        String mimeType = audioFormat.getString(MediaFormat.KEY_MIME);
+
+        MediaCodec codec = null;
+        FileOutputStream outputStream = null;
+
+        try {
+            codec = MediaCodec.createDecoderByType(mimeType);
+            codec.configure(audioFormat, null, null, 0);
+            codec.start();
+
+            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+            boolean isExtractorEOS = false;
+            boolean isDecoderEOS = false;
+
+            outputStream = new FileOutputStream(outputAudioFile);
+
+            while (!isDecoderEOS) {
+                if (!isExtractorEOS) {
+                    int inputBufferIndex = codec.dequeueInputBuffer(10000);
+                    if (inputBufferIndex >= 0) {
+                        ByteBuffer inputBuffer = codec.getInputBuffer(inputBufferIndex);
+                        int sampleSize = extractor.readSampleData(inputBuffer, 0);
+                        if (sampleSize < 0) {
+                            codec.queueInputBuffer(inputBufferIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
+                            isExtractorEOS = true;
+                        } else {
+                            codec.queueInputBuffer(inputBufferIndex, 0, sampleSize, extractor.getSampleTime(), 0);
+                            extractor.advance();
+                        }
+                    }
+                }
+
+                int outputBufferIndex = codec.dequeueOutputBuffer(bufferInfo, 10000);
+                if (outputBufferIndex >= 0) {
+                    ByteBuffer outputBuffer = codec.getOutputBuffer(outputBufferIndex);
+                    byte[] chunk = new byte[bufferInfo.size];
+                    outputBuffer.get(chunk);
+                    outputBuffer.clear();
+                    outputStream.write(chunk);
+                    codec.releaseOutputBuffer(outputBufferIndex, false);
+                    if ((bufferInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+                        isDecoderEOS = true;
+                    }
+                } else if (outputBufferIndex == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                    // Ignore format change
+                }
+            }
+
+            callback.onExtractionCompleted(outputAudioFile);
+        } catch (IOException e) {
+            callback.onExtractionFailed("Audio extraction failed: " + e.getMessage());
+        } finally {
+            if (codec != null) {
+                codec.stop();
+                codec.release();
+            }
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            extractor.release();
+        }
+    }
+
+    private static int findAudioTrack(MediaExtractor extractor) {
+        int numTracks = extractor.getTrackCount();
+        for (int i = 0; i < numTracks; i++) {
+            MediaFormat format = extractor.getTrackFormat(i);
+            String mime = format.getString(MediaFormat.KEY_MIME);
+            if (mime.startsWith("audio/")) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- android/src/main/java/com/bazumax/capacitor/audio/from/video/AudioFromVideoRetrieverPlugin.java
+++ android/src/main/java/com/bazumax/capacitor/audio/from/video/AudioFromVideoRetrieverPlugin.java
@@ -1,22 +1,66 @@
 package com.bazumax.capacitor.audio.from.video;
 
+import android.Manifest;
+import android.content.ContentResolver;
+
 import com.getcapacitor.JSObject;
+import com.getcapacitor.PermissionState;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.annotation.Permission;
+import com.getcapacitor.annotation.PermissionCallback;
 
-@CapacitorPlugin(name = "AudioFromVideoRetriever")
+import java.io.File;
+
+@CapacitorPlugin(name = "AudioFromVideoRetriever", permissions = {
+    @Permission(
+            alias = "storage",
+            strings = {
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }
+    )
+})
 public class AudioFromVideoRetrieverPlugin extends Plugin {
 
     private AudioFromVideoRetriever implementation = new AudioFromVideoRetriever();
 
-    @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    @PermissionCallback
+    private void videoPermsCallback(PluginCall call) {
+        if (getPermissionState("storage") == PermissionState.GRANTED) {
+            extractAudio(call);
+        } else {
+            call.reject("Permission is required to take a picture");
+        }
+    }
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+    @PluginMethod
+    public void extractAudio(PluginCall call) {
+        if (getPermissionState("storage") != PermissionState.GRANTED) {
+            requestPermissionForAlias("storage", call, "videoPermsCallback");
+            return;
+        }
+        String path = call.getString("path");
+        String outputPath = call.getString("outputPath");
+
+        ContentResolver resolver = bridge.getContext().getContentResolver();
+        File inputFile = implementation.getFileObject(path, resolver);
+        File outputFile = implementation.getFileObject(outputPath, resolver);
+
+        implementation.extractAudio(inputFile, outputFile, new AudioFromVideoRetriever.ExtractionCallback() {
+            @Override
+            public void onExtractionCompleted(File audioFile) {
+                JSObject ret = new JSObject();
+                ret.put("path", outputPath);
+                call.resolve(ret);
+            }
+
+            @Override
+            public void onExtractionFailed(String errorMessage) {
+                call.reject(errorMessage);
+            }
+        });
     }
 }
